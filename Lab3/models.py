56bf33f4c53b6aa209cc64824732a12d2143f0f0
Evenlaxxus <r.ewiak1404@wp.pl>
2020-06-13 20:19:44 +0200

    Lab3 delete comments

--- Lab3/models.py
+++ Lab3/models.py
@@ -16,22 +16,5 @@ def getRow(user_genre_rating, index):
     return json.loads(user_genre_rating.to_json(orient='records'))[index]
 
 
-# def get_this_shit(nrows):
-#     DFUserRatedMovies = pd.read_csv('../user_ratedmovies.dat', header=0, delimiter='\t',
-#                                     usecols=['userID', 'movieID', 'rating'], nrows=nrows)
-#     DFMovieGenres = pd.read_csv('../movie_genres.dat', header=0, delimiter="\t")
-#     DFMovieGenres['dummyColumn'] = int(1)
-#     DFMovieGenresPivoted = DFMovieGenres.pivot_table(index='movieID', columns='genre', values='dummyColumn')
-#     DFMovieGenresPivotedColumnNamesMap = {}
-#     genres_column_names = []
-#     for DFMovieGenresPivotedColumnName in DFMovieGenresPivotedColumnNamesMap:
-#         another_genre_column_name = 'gnere-' + DFMovieGenresPivotedColumnName
-#         DFMovieGenresPivotedColumnNamesMap[DFMovieGenresPivotedColumnName] = another_genre_column_name
-#         genres_column_names.append(another_genre_column_name)
-#     DFMovieGenresPivoted = DFMovieGenresPivoted.rename(columns=DFMovieGenresPivotedColumnNamesMap)
-#     DFUserRatedMoviesGenres = pd.merge(DFUserRatedMovies, DFMovieGenresPivoted, on='movieID')
-#     return DFUserRatedMoviesGenres, genres_column_names
-
-
 if __name__ == "__main__":
     print(getData())
