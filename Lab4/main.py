364ecfabee1451b1d57d4f327ca26dcd408c0a38
Evenlaxxus <r.ewiak1404@wp.pl>
2020-06-14 23:24:58 +0200

    Lab4 zad6

--- Lab4/main.py
+++ Lab4/main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 
 def zad1():
@@ -40,5 +39,24 @@ def zad5():
     return genre_ratings
 
 
+def zad6(userID):
+    user_rated_movies, genres = zad1()
+    genre_user_ratings = {"userID": userID}
+    for genre in genres:
+        ratings = []
+        for index, row in user_rated_movies.iterrows():
+            if row['userID'] == userID:
+                if row[genre] == 1.0:
+                    ratings.append(row['rating'])
+        if len(ratings) != 0:
+            genre_user_ratings[genre] = sum(ratings) / len(ratings)
+        else:
+            genre_user_ratings[genre] = 0
+    return genre_user_ratings
+
+
 if __name__ == "__main__":
-    print(zad5())
+    # zad4()
+    # print(zad5())
+    print(zad6(78))
+
